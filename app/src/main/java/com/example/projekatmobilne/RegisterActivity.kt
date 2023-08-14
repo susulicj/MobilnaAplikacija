488d370f3bdcfefd99bcaeda4905f95b0868ea86
Jovana <suslic.jovana@elfak.rs>
2023-08-14 23:54:15 +0200

    Azuriranje

--- app/src/main/java/com/example/projekatmobilne/RegisterActivity.kt
+++ app/src/main/java/com/example/projekatmobilne/RegisterActivity.kt
@@ -33,19 +33,19 @@ class RegisterActivity : AppCompatActivity() {
 
     }
 
-    private fun performSignUp(){
+    private fun performSignUp() {
         var email = binding.etEmail.text.toString()
-        var korisnickoIme  = binding.ptKorisnickoIme.text.toString()
+        var korisnickoIme = binding.ptKorisnickoIme.text.toString()
         var password = binding.passwordReg.text.toString()
         var imeIprezime = binding.etImeiPrezime.text.toString()
         var brojTelefona = binding.editTextNumber.text.toString()
-        val user= hashMapOf(
+        val user = hashMapOf(
             "Email" to email,
             "Korisnicko ime" to korisnickoIme,
             "Broj telefona" to brojTelefona,
             "Ime i prezime" to imeIprezime,
 
-        )
+            )
 
         if (email.isEmpty() || korisnickoIme.isEmpty() || password.isEmpty() || imeIprezime.isEmpty() || brojTelefona.isEmpty()) {
             Toast.makeText(this, "Please, fill all fields", Toast.LENGTH_LONG).show()
@@ -54,53 +54,22 @@ class RegisterActivity : AppCompatActivity() {
 
         val Users = db.collection("USERS")
         val query = Users.whereEqualTo("Email", email).get()
-                         .addOnSuccessListener {
-                             tasks->
-                             if(tasks.isEmpty)
-                             {
-                                 auth.createUserWithEmailAndPassword(email, password)
-                                     .addOnCompleteListener(this){
-                                         task->
-                                         if(task.isSuccessful)
-                                         {
-                                             Users.document(email).set(user)
-                                         }
-                                         else
-                                         {
-                                             Toast.makeText(this, "Failed", Toast.LENGTH_LONG).show()
-                                         }
-                                     }
-                             }
-                             else
-                             {
-                                 Toast.makeText(this, "Vec postoji korisnik", Toast.LENGTH_LONG).show()
-                             }
-                         }
-
-
-
-        /*auth.createUserWithEmailAndPassword(korisnickoIme, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d("TAG", "createUserWithEmail:success")
-                    val user = auth.currentUser
-
+            .addOnSuccessListener { tasks ->
+                if (tasks.isEmpty) {
+                    auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                Users.document(email).set(user)
+                            } else {
+                                Toast.makeText(this, "Failed", Toast.LENGTH_LONG).show()
+                            }
+                        }
                 } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w("TAG", "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext,
-                        "Authentication failed.",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-
+                    Toast.makeText(this, "Vec postoji korisnik", Toast.LENGTH_LONG).show()
                 }
+            }
 
-            }.addOnFailureListener{
-                Toast.makeText(this, "Failer", Toast.LENGTH_LONG).show()
-            }*/
+
     }
-
 
 }
